544df89ca0fde3b67916d9f73fa25daf6bccfe2f
Nico <nicholasloperena@gmail.com>
2024-05-08 20:59:30 -0400

    Javascript I learned about

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("Hello World!")
+
+let fullName = "Nicholas Loperena"
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit)
+
+let cash = 45;
+let price = 40;
+let difference = cash - price
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${difference} your change`)
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+else {
+    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+}

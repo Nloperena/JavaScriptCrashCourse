9a58dd95211144173a1427ce2b65b6a36c19487a
Nico <nicholasloperena@gmail.com>
2024-05-13 22:31:08 -0400

    did another exercise from the Crashcourse

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ console.log(fahrenheit)
 
 let cash = 45;
 let price = 40;
+let isStoreOpen = true;
 let difference = cash - price
 
-if (cash > price) {
+if (cash >= price) {
     console.log(`you paid extra - here's ${difference} your change`)
 }
 else if (cash === price) {
@@ -19,3 +20,16 @@ else if (cash === price) {
 else {
     console.log(`not enough money - you still owe ${difference * -1} dollars`)
 }
+
+
+let hot = true
+hot ? console.log("weather is hot outside") : console.log("weather is cold")
+
+let subscribed = true
+let loggedIn = true
+
+let str = subscribed || loggedIn ? "show the video" : "hide the video"
+console.log(str);
+
+let receipt = isStoreOpen && (cash >= price) ? "give receipt" : "do not give receipt"
+console.log(receipt)

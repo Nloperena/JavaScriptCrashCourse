4507ef5321080b3829402d8e8eaf77a29dc8a08b
NLoperena <nicholasloperena@gmail.com>
2024-05-28 13:31:26 -0400

    did the same method but through a for loop

--- app.js
+++ app.js
@@ -138,5 +138,16 @@
 
 let grades = ['A+', 'A', 'FAIL']
 
-let goodGrades = grades.filter(element => element != 'FAIL');
+// let goodGrades = grades.filter(element => element != 'FAIL');
+// console.log(goodGrades)
+
+//Create new empty array
+let goodGrades = []
+
+for (let i = 0; i < grades.length; ++i) {
+    if (grades[i] !== 'FAIL') {
+        goodGrades.push(grades[i]);
+    }
+}
+
 console.log(goodGrades)

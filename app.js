d78eace198f0cb9883f36d3ec1f39e95d56d96ce
NLoperena <nicholasloperena@gmail.com>
2024-05-28 13:21:23 -0400

    practiced the .filter method to filter out grades that were not failures

--- app.js
+++ app.js
@@ -78,12 +78,65 @@
 
 // FUNCTIONS
 
-// Function definition
-function welcomePersonToFES(firstName, lastName) {
-    console.log (`Welcome to FES, ${firstName} ${lastName}`)
-}
+// // Function definition
+// function welcomePersonToFES(firstName, lastName) {
+//     console.log (`Welcome to FES, ${firstName} ${lastName}`)
+// }
 
-//Call the function
-welcomePersonToFES(`Joe`, `Loperena`);
-welcomePersonToFES(`Nick`, `Loperena`);
-welcomePersonToFES(`Jay`, `Loperena`);
+// //Call the function
+// welcomePersonToFES(`Joe`, `Loperena`);
+// welcomePersonToFES(`Nick`, `Loperena`);
+// welcomePersonToFES(`Jay`, `Loperena`);
+
+// // Addition function
+// // function sumOfTwoNumbers(num1, num2) {
+// //    return num1 + num2
+// // }
+
+// // console.log(sumOfTwoNumbers(10, 20))
+
+// // Create a function that converts Celsius to Fahrenheit
+// function celsiusToFahrenheit(celsius) {
+//     return celsius * 1.8 + 32
+// }
+
+// console.log(celsiusToFahrenheit(0))
+// console.log(celsiusToFahrenheit(10))
+// console.log(celsiusToFahrenheit(30))
+
+
+// ARRAYS
+
+// let arr = [20, 30, 40, 50, 100]
+
+// // First element of array:
+// console.log(arr[0])
+
+// // Last element of array:
+// console.log(arr[4])
+
+// console.log(arr[arr.length - 1])
+// // Adding an element to the end of an array
+// arr.push(200)
+
+
+// console.log(arr)
+
+// let newArr = arr.filter((element) => {
+//     console.log(element)
+//     if (element < 50) {
+//         return true
+//     }
+// })
+
+// console.log(newArr)
+
+
+// studentA = ['A+', 'A', 'FAIL'] 
+// studentB = ['FAIL', 'FAIL', 'B']
+// studentC = ['FAIL', ]
+
+let grades = ['A+', 'A', 'FAIL']
+
+let goodGrades = grades.filter(element => element != 'FAIL');
+console.log(goodGrades)
